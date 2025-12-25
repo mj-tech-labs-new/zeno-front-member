279ae196991f7d911cd7fcb6e20a719a87e716c4
Parth Gondaliya <parth.gondaliya@mjtechlabs.com>
2025-12-25 10:04:47 +0530

    fix: scroll issue token dropdown

--- src/pages/ChartPages/components/TokenDropdown.tsx
+++ src/pages/ChartPages/components/TokenDropdown.tsx
@@ -71,7 +71,7 @@ const TokenDropdown = () => {
     <React.Fragment>
       <div
         ref={mainDivRef}
-        className="flex items-center gap-2 cursor-pointer"
+        className="flex items-center gap-2 cursor-pointer "
         onClick={(e) => {
           e.stopPropagation()
           setIsDivOpen((data) => !data)
@@ -91,7 +91,7 @@ const TokenDropdown = () => {
       {isDivOpen && (
         <div
           ref={floatingDiv}
-          className="fixed inset-0 z-[999] bg-button-tertiary-color rounded-md left-0 w-[500px] p-5"
+          className="fixed inset-0 z-[999] bg-button-tertiary-color rounded-md left-0 w-[500px] p-5  "
         >
           <SearchComponent
             ref={searchRef}
@@ -101,67 +101,69 @@ const TokenDropdown = () => {
             setSearchValue={setSearchValue}
           />
 
-          <div className="mt-5">
-            <div className="grid grid-cols-3 gap-2.5 *:text-neutral-primary-color font-semibold text-sm px-2.5">
-              <span>{English.E386}</span>
-              <p className="text-right">{English.E387}</p>
-              <p className="text-right">{English.E119}</p>
-            </div>
-            {tokenArray?.map((item) => {
-              const {id, token_image_url, token_symbol} = item
-              const tokenData = totalTokenData?.[token_symbol]
-              return (
-                <div
-                  key={id}
-                  className="grid grid-cols-3 gap-2.5 text-primary-color font-medium tracking-wider hover:bg-neutral-secondary-color px-2.5 py-4 rounded-md cursor-pointer overflow-y-auto"
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    setSelectedToken((data) => {
-                      if (data?.token_symbol !== item?.token_symbol) {
-                        CommonFunction.addSliceData('removeCoinToken', '')
-                        CommonFunction.addSliceData('addCoinToken', {
-                          token: item?.token_name?.replace('USDT', ''),
-                        })
-                        CommonFunction.addSliceData('addAmountType', {
-                          amount: chartInfo?.symbol,
-                        })
-                        isLastCandle.current = false
-                        const matchCoin = tokenList?.find(
-                          (token) => token.token_symbol === item?.token_symbol
-                        )
-                        totalCandlesCount.current = 0
-                        return matchCoin ?? null
-                      }
-                      return data
-                    })
-                    setIsDivOpen(false)
-                    setSearchValue('')
-                  }}
-                >
-                  <div className="gap-2 flex items-center">
-                    <ImageComponent
-                      className="w-6 h-6"
-                      imageUrl={`${import.meta.env.VITE_API_BASE_URL_PRODUCTION}${token_image_url?.replace('/home/ubuntu/backend/', '')}`}
-                    />
-                    <p className="text-left">{token_symbol + English.E60}</p>
+          <div className="grid grid-cols-3 mt-5 gap-2.5 *:text-neutral-primary-color font-semibold text-sm px-2.5">
+            <span>{English.E386}</span>
+            <p className="text-right">{English.E387}</p>
+            <p className="text-right">{English.E119}</p>
+          </div>
+          <div className="mt-5 ">
+            <div className="overflow-y-auto h-[calc(100%-80px)]">
+              {tokenArray?.map((item) => {
+                const {id, token_image_url, token_symbol} = item
+                const tokenData = totalTokenData?.[token_symbol]
+                return (
+                  <div
+                    key={id}
+                    className="grid grid-cols-3  gap-2.5 text-primary-color font-medium tracking-wider hover:bg-neutral-secondary-color px-2.5 py-4 rounded-md cursor-pointer overflow-y-auto"
+                    onClick={(e) => {
+                      e.stopPropagation()
+                      setSelectedToken((data) => {
+                        if (data?.token_symbol !== item?.token_symbol) {
+                          CommonFunction.addSliceData('removeCoinToken', '')
+                          CommonFunction.addSliceData('addCoinToken', {
+                            token: item?.token_name?.replace('USDT', ''),
+                          })
+                          CommonFunction.addSliceData('addAmountType', {
+                            amount: chartInfo?.symbol,
+                          })
+                          isLastCandle.current = false
+                          const matchCoin = tokenList?.find(
+                            (token) => token.token_symbol === item?.token_symbol
+                          )
+                          totalCandlesCount.current = 0
+                          return matchCoin ?? null
+                        }
+                        return data
+                      })
+                      setIsDivOpen(false)
+                      setSearchValue('')
+                    }}
+                  >
+                    <div className="gap-2 flex items-center">
+                      <ImageComponent
+                        className="w-6 h-6"
+                        imageUrl={`${import.meta.env.VITE_API_BASE_URL_PRODUCTION}${token_image_url?.replace('/home/ubuntu/backend/', '')}`}
+                      />
+                      <p className="text-left">{token_symbol + English.E60}</p>
+                    </div>
+                    {tokenData?.high ? (
+                      <p className="text-right">
+                        {Utility.numberConversion(Number(tokenData?.high ?? 0))}
+                      </p>
+                    ) : (
+                      '----'
+                    )}
+                    {tokenData?.change ? (
+                      <span
+                        className={`text-right ${Utility.colorGeneratorUtility(Number(tokenData?.change ?? 0))}`}
+                      >{`${Utility.numberConversion(Number(tokenData?.change ?? 0))}%`}</span>
+                    ) : (
+                      '----'
+                    )}
                   </div>
-                  {tokenData?.high ? (
-                    <p className="text-right">
-                      {Utility.numberConversion(Number(tokenData?.high ?? 0))}
-                    </p>
-                  ) : (
-                    '----'
-                  )}
-                  {tokenData?.change ? (
-                    <span
-                      className={`text-right ${Utility.colorGeneratorUtility(Number(tokenData?.change ?? 0))}`}
-                    >{`${Utility.numberConversion(Number(tokenData?.change ?? 0))}%`}</span>
-                  ) : (
-                    '----'
-                  )}
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
           </div>
         </div>
       )}
