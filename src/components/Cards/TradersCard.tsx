e59bf53fd99459fbf2c8a14d5e9da18061b99d52
Vatsal-Chauhan024 <adarsh_vc24@yahoo.com>
2026-01-09 19:34:01 +0530

    fix: card re arranged

--- src/components/Cards/TradersCard.tsx
+++ src/components/Cards/TradersCard.tsx
@@ -1,49 +1,52 @@
 import {TraderCardsProps} from '@/types/ComponentTypes'
 
 import DescriptionComponent from '../DescriptionComponent/DescriptionComponent'
+import OpacityContainer from '../GsapComponents/OpacityContainer'
 import ImageComponent from '../ImageComponent/ImageComponent'
 
 const TradersCard = (props: TraderCardsProps) => {
   const {description, details, mainImg, mainTitle, secondaryDesc} = props
 
   return (
-    <div className="card  overflow-hidden">
-      <div className=" p-4 xl:p-8 mt-[100px] bg-dark-gray-bg rounded-[26px] lg:p-[85px] lg:pr-0! grid grid-cols-1 lg:grid-cols-2 gap-[49px]">
-        <div className="flex flex-col gap-12 xl:gap-[96px] justify-between">
-          <div className="flex flex-col gap-3 *:font-geist!">
-            <DescriptionComponent
-              className="font-medium last:mt-0 text-base/6! xl:text-[40px]/12! text-primary-color!"
-              multilineContent={mainTitle}
-            />
-            <DescriptionComponent
-              className="text-base/6 xl:text-lg/[30px] font-light font-geist! text-primary-color/50"
-              singleLineContent={description}
-            />
-          </div>
+    <OpacityContainer>
+      <div className="card  overflow-hidden">
+        <div className=" p-4 xl:p-8 bg-dark-gray-bg rounded-[26px] lg:p-[85px] lg:pr-0! grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-[49px]">
+          <div className="flex flex-col gap-12 xl:gap-[96px] justify-between">
+            <div className="flex flex-col gap-3 *:font-geist!">
+              <DescriptionComponent
+                className="font-medium last:mt-0 text-base/6! xl:text-[40px]/12! text-primary-color!"
+                multilineContent={mainTitle}
+              />
+              <DescriptionComponent
+                className="text-base/6 xl:text-lg/[30px] font-light font-geist! text-primary-color/50"
+                singleLineContent={description}
+              />
+            </div>
 
-          <div className="flex-col gap-3.5 xl:flex hidden">
-            <span className="text-lg/5 font-light text-primary-color/50 font-geist!">
-              {secondaryDesc}
-            </span>
-            <div className="flex gap-2 flex-wrap">
-              {details?.map((item) => (
-                <p
-                  key={item}
-                  className="py-2 pl-2 pr-3 rounded-[4px] border border-solid border-white/30 text-base/5 font-light font-inter! text-primary-color text-left"
-                >
-                  {item}
-                </p>
-              ))}
+            <div className="flex-col gap-3.5 xl:flex hidden">
+              <span className="text-lg/5 font-light text-primary-color/50 font-geist!">
+                {secondaryDesc}
+              </span>
+              <div className="flex gap-2 flex-wrap">
+                {details?.map((item) => (
+                  <p
+                    key={item}
+                    className="py-2 pl-2 pr-3 rounded-[4px] border border-solid border-white/30 text-base/5 font-light font-inter! text-primary-color text-left"
+                  >
+                    {item}
+                  </p>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
 
-        <ImageComponent
-          className="[&>img]:custom_shadow [&>img]:object-contain h-[220px] xl:h-[440px]"
-          imageUrl={mainImg}
-        />
+          <ImageComponent
+            className="[&>img]:custom_shadow [&>img]:object-contain  lg:h-[440px]"
+            imageUrl={mainImg}
+          />
+        </div>
       </div>
-    </div>
+    </OpacityContainer>
   )
 }
 
--- src/pages/NewHomePage/components/Stacking.tsx
+++ src/pages/NewHomePage/components/Stacking.tsx
@@ -1,54 +1,54 @@
-import {useGSAP} from '@gsap/react'
-import gsap from 'gsap'
-import {ScrollSmoother} from 'gsap/all'
-import _ScrollTrigger, {ScrollTrigger} from 'gsap/ScrollTrigger'
+// import {useGSAP} from '@gsap/react'
+// import gsap from 'gsap'
+// import {ScrollSmoother} from 'gsap/all'
+// import _ScrollTrigger, {ScrollTrigger} from 'gsap/ScrollTrigger'
 import {useRef} from 'react'
 
 import TradersCard from '@/components/Cards/TradersCard'
 import {Constants} from '@/helpers'
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
 const Stacking = () => {
   const containerRef = useRef(null)
   const wrapperRef = useRef<HTMLDivElement | null>(null)
 
-  useGSAP(
-    () => {
-      const cards = gsap.utils.toArray('.card')
+  // useGSAP(
+  //   () => {
+  //     const cards = gsap.utils.toArray('.card')
 
-      cards.forEach((card, index) => {
-        gsap.to(card as any, {
-          scrollTrigger: {
-            trigger: card as any,
-            start: () => `top bottom-=100`,
-            end: () => `top top+=40`,
-            scrub: true,
-            invalidateOnRefresh: true,
-          },
-          ease: 'none',
-          endTrigger: containerRef.current,
-          end: 'bottom top',
-          scale: () => 1 - (cards.length - index) * 0.025,
-        })
-        ScrollTrigger.create({
-          trigger: card as any,
-          start: 'top top',
-          pin: true,
-          pinSpacing: false,
-          endTrigger: containerRef.current,
-          end: 'bottom top',
-          id: 'pin',
-          invalidateOnRefresh: true,
-        })
-      })
-    },
-    {scope: containerRef}
-  )
+  //     cards.forEach((card, index) => {
+  //       gsap.to(card as any, {
+  //         scrollTrigger: {
+  //           trigger: card as any,
+  //           start: () => `top bottom-=100`,
+  //           end: () => `top top+=40`,
+  //           scrub: true,
+  //           invalidateOnRefresh: true,
+  //         },
+  //         ease: 'none',
+  //         endTrigger: containerRef.current,
+  //         end: 'bottom top',
+  //         scale: () => 1 - (cards.length - index) * 0.025,
+  //       })
+  //       ScrollTrigger.create({
+  //         trigger: card as any,
+  //         start: 'top top',
+  //         pin: true,
+  //         pinSpacing: false,
+  //         endTrigger: containerRef.current,
+  //         end: 'bottom top',
+  //         id: 'pin',
+  //         invalidateOnRefresh: true,
+  //       })
+  //     })
+  //   },
+  //   {scope: containerRef}
+  // )
 
   return (
     <div ref={wrapperRef} className="">
-      <div ref={containerRef} className="flex flex-col">
+      <div ref={containerRef} className="flex flex-col gap-8">
         {Constants.StackingCard.map((item) => (
           <TradersCard {...item} key={item.description} />
         ))}
