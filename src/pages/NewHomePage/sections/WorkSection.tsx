5a87a02c26d45b52729e46eee824c363a8ba7e40
Vatsal-Chauhan024 <adarsh_vc24@yahoo.com>
2026-01-11 15:03:49 +0530

    fix: home page work section fixed

--- src/pages/NewHomePage/sections/WorkSection.tsx
+++ src/pages/NewHomePage/sections/WorkSection.tsx
@@ -1,12 +1,8 @@
-import { useEffect, useRef, useState } from 'react'
+import {useEffect, useRef, useState} from 'react'
 
-import {
-  Accordian,
-  ImageComponent,
-  WordSplit,
-} from '@/components'
-import { Constants, English, Images } from '@/helpers'
-import { useClickOutside } from '@/hooks'
+import {Accordian, ImageComponent, WordSplit} from '@/components'
+import {Constants, English, Images} from '@/helpers'
+import {useClickOutside} from '@/hooks'
 
 const WorkSection = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
@@ -30,39 +26,37 @@ const WorkSection = () => {
 
   return (
     <div
-      className=" min-h-[920px] flex items-center bg-linear-to-r from-light-gr1 via-light-gr2 to-light-gr3"
+      className="py-[120px] pl-5 xl:pl-52 pt-[118px] bg-linear-to-r from-light-gr1 via-light-gr2 to-light-gr3"
       id="how_it_works"
     >
-      <div ref={sectionRef} className="flex items-center py-[120px] pl-5 xl:pl-52">
+      <div ref={sectionRef} className="flex items-center ">
         <div className="flex flex-col lg:flex-row gap-2 h-full ">
-          <div className="flex flex-col justify-between gap-5 w-full shrink-0 max-w-[590px] xl:pr-[110px]">
+          <div className="flex flex-col gap-5 w-full shrink-0 xl:max-w-[590px] xl:pr-[110px]">
             <WordSplit
               className="text-primary-black!"
               singleLineContent={English.E400}
             />
-            <div className="flex items-center">
-              <div className="w-full mt-auto h-[500px] z-30 flex">
-                <div className="h-fit mt-auto">
-                  {Constants.QA.map((qa, index) => (
-                    <div key={qa.que} className="">
-                      <Accordian
-                        isLineType
-                        className="!gap-0"
-                        indexValue={index + 1}
-                        isOpen={currentIndex === index}
-                        multilineContent={[qa.ans]}
-                        singleLineContent={qa.que}
-                        onPressItem={() => {
-                          setCurrentIndex(index)
-                        }}
-                      />
-                    </div>
-                  ))}
-                </div>
+            <div className="h-full flex items-center xl:relative">
+              <div className="h-fit mt-auto w-full xl:absolute xl:bottom-0 xl:z-30">
+                {Constants.QA.map((qa, index) => (
+                  <div key={qa.que} className="flex">
+                    <Accordian
+                      isLineType
+                      className="!gap-0 w-full"
+                      indexValue={index + 1}
+                      isOpen={currentIndex === index}
+                      layoutClassName="flex-1"
+                      multilineContent={[qa.ans]}
+                      singleLineContent={qa.que}
+                      onPressItem={() => {
+                        setCurrentIndex(index)
+                      }}
+                    />
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-
 
           <ImageComponent
             className="flex items-center justify-center  custom_shadow overflow-hidden rounded-l-[24px] z-20"
